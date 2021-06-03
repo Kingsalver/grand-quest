d7aaf9b85397f0418348a7dd3faf8122cd19cfa0
22bparr <ben4parr@gmail.com>
2021-06-03 08:37:55 -0400

    Entity

---
+++ src/Entity.java
@@ -0,0 +1,33 @@
+public class Entity {
+    private int HP;
+    private int power;
+
+    public Entity (){
+        
+    }
+
+
+
+
+
+
+
+
+    public int getHP(){
+        return HP;
+    }
+
+    public void setHP(int HP){
+        this.HP = HP;
+    }
+
+    public int getPower(){
+        return power;
+    }
+
+    public void setPower(int power){
+        this.power = power;
+    }
+
+
+}
